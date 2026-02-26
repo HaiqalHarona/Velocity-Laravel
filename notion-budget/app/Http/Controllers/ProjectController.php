f8d30e6f7d63890b5e1ddff36a06f69d390ce7fc
FridasLaves <haronahaiqal@gmail.com>
2026-02-26 13:03:42 +0800

    Project Controller Create

---
+++ notion-budget/app/Http/Controllers/ProjectController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProjectController
+{
+    // Display project view with proj_id and hashed routes
+    public function ProjectView()
+    {
+        // return view('projects.index');
+    }
+
+    public function ProjectCreate()
+    {
+
+    }
+
+    public function ProjectDelete()
+    {
+
+    }
+
+    public function ProjectUpdate()
+    {
+    }
+
+    public function ProjectEdit()
+    {
+
+    }
+}
